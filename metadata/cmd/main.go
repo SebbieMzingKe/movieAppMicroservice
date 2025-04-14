06159160ff96153378dc5578b53611212f8e3cfc
SebbieMzingKe <sebbievilar2@gmail.com>
2025-04-14 22:20:42 +0300

    update(chore): add main.go

--- metadata/cmd/main.go
+++ metadata/cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"movieapp.com/metadata/internal/controller/metadata"
+	httphandler "movieapp.com/metadata/internal/handler/http"
+	"movieapp.com/metadata/internal/repository/memory"
+)
+
 func main() {
+	log.Println("Starting the movie metadata service")
+	repo := memory.New()
+	ctrl := metadata.New(repo)
+	h := httphandler.New(ctrl)
+	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata))
 	
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		panic(err)
+	}
 }
