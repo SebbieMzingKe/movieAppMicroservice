172c8eb7df3373c155d934268f6eb9b2366de330
SebbieMzingKe <sebbievilar2@gmail.com>
2025-05-19 11:36:13 +0300

    feat: create repository for rating service

---
+++ rating/internal/repository/memory/memory.go
@@ -0,0 +1,28 @@
+package memory
+
+import (
+	"context"
+
+	"movieapp.com/rating/internal/repository"
+	model "movieapp.com/rating/pkg"
+)
+
+type Repository struct {
+	data map[model.RecordType]map[model.RecordId][]model.Rating
+}
+
+func New() *Repository {
+	return &Repository{map[model.RecordType]map[model.RecordId][]model.Rating{}}
+}
+
+func (r *Repository) Get(ctx context.Context, recordId model.RecordId, recordType model.RecordType) ([]model.Rating, error) {
+	if _, ok := r.data[recordType]; !ok {
+		return nil, repository.ErrNotFound
+	}
+
+	if ratings, ok := r.data[recordType][recordId]; ok || len(ratings) == 0 {
+		return nil, repository.ErrNotFound
+	}
+
+	return r.data[recordType][recordId], nil
+}
