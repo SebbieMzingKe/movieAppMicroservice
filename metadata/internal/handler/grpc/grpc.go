aa03a5aa626bf6d23fd5f4ea918b4c2b7cf1a13e
SebbieMzingKe <sebbievilar2@gmail.com>
2025-06-08 22:48:34 +0300

    feat(gateways): finalize on get mtadata by id

--- metadata/internal/handler/grpc/grpc.go
+++ metadata/internal/handler/grpc/grpc.go
@@ -1,15 +1,35 @@
 package grpc
 
 import (
+	"context"
+	"errors"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"movieapp.com/gen"
 	"movieapp.com/metadata/internal/controller/metadata"
+	"movieapp.com/metadata/pkg/model"
 )
 
 type Handler struct {
 	gen.UnimplementedMetadataServiceServer
-	ctrl *metadata.Controller
+	svc *metadata.Controller
 }
 
 func New(ctrl *metadata.Controller) *Handler {
-	return &Handler{ctrl: ctrl}
+	return &Handler{svc: ctrl}
+}
+
+func (h *Handler) GetMetadataByID(ctx context.Context, req *gen.GetMetadataRequest) (*gen.GetMetadataResponse, error) {
+	if req == nil || req.MovieId == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "nil req orempty id")
+	}
+
+	m, err := h.svc.Get(ctx, req.MovieId)
+	if err != nil && errors.Is(err, metadata.ErrNotFound) {
+		return nil, status.Errorf(codes.NotFound, "%s", err.Error())
+	} else if err != nil {
+		return nil, status.Errorf(codes.Internal, "%s", err.Error())
+	}
+	return &gen.GetMetadataResponse{Metadata: model.MetadataToProto(m)}, nil	
 }
